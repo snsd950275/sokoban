dfe3902a54e2edf61a3355d02743a446f85f2d4c
snsd950275 <snsd950275@gmail.com>
2017-11-03 14:44:26 +0800

    ...

--- httpd/index.js
+++ httpd/index.js
@@ -1,19 +1,38 @@
 'use strict';
 let http = require('http');
 http.createServer((request, response) => {
+let fs = require('fs');
+let postData = ''; // POST 資料
+// 利⽤'data' event 消耗掉data chunk;
+// 'end' event 才會被fired
+request.on('data', (chunk) => {
+postData += chunk;
+console.log(
+' 接收的POST data ⽚段k: [' + chunk + '].'
+);
+});
 request.on('end', () => {
-console.log('Request method: ' + request.method);
-console.log('Request url: ' + request.url);
-});
-
-// 傳送HTTP header
-// HTTP Status: 200 : OK
-// Content Type: text/plain
+switch (request.url) {
+case '/':
+fs.readFile('../htdocs/index.html', (err, data) => {
+if (err) {
+console.log(' 檔案讀取錯誤');
+}
+else {
 response.writeHead(200, {
-'Content-Type': 'text/plain'
+'Content-Type': 'text/html'
 });
-// 傳送回應內容。
-response.end('Hello World!\n');
+response.write(data);
+response.end();
+}
+});
+break;
+default:
+console.log(' 未定義的存取: ' + request.url);
+response.end();
+break;
+}
+});
 }).listen(8088);
 // log message to Console
-console.log('Server running at http://127.0.0.1:8088/');
+console.log(' 伺服器啓動，連線url: http://127.0.0.1:8088/');
