ca543bfbcf781698d5beb4ea52b5eeaf0c7ade07
snsd950275 <snsd950275@gmail.com>
2017-11-03 13:40:49 +0800

    ...

--- httpd/index.js
+++ httpd/index.js
@@ -1,10 +1,19 @@
- 'use strict';
- let http = require('http');
-  http.createServer((request, response) => {
-   response.writeHead(200, {
-    'Content-Type': 'text/plain'
-     });
-      response.end('Hello World!\n');
-       console.log('request.headers: \n', request.headers)
-        }).listen(8088);
-        console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/'); 
+'use strict';
+let http = require('http');
+http.createServer((request, response) => {
+request.on('end', () => {
+console.log('Request method: ' + request.method);
+console.log('Request url: ' + request.url);
+});
+
+// 傳送HTTP header
+// HTTP Status: 200 : OK
+// Content Type: text/plain
+response.writeHead(200, {
+'Content-Type': 'text/plain'
+});
+// 傳送回應內容。
+response.end('Hello World!\n');
+}).listen(8088);
+// log message to Console
+console.log('Server running at http://127.0.0.1:8088/');
